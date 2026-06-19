46e849e14a844159c9fa45aa7ee4943f60b0406a
root <2813767513@qq.com>
2026-06-20 01:31:17 +0800

    验证：完成 v0.2 真实功能验收

--- server.js
+++ server.js
@@ -199,7 +199,7 @@ const server = createServer(async (request, response) => {
       sendJson(response, 200, {
         ok: true,
         name: "Claudio",
-        version: "0.1.0"
+        version: "0.2.0"
       });
       return;
     }
